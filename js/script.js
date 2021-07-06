0a9f863e2f705bf6bce36526fdd902805c5b9dff
noemi86-cpu <noemipatermo@gmail.com>
2021-07-06 12:19:06 +0200

    first-milestone

---
+++ js/script.js
@@ -0,0 +1,43 @@
+Vue.config.devtools = true;
+new Vue (
+    {
+        el:"#app",
+        data:{
+            jobs: [
+                {
+                  id: 1,
+                  company: 'Perferendis',
+                  position: 'Developer',
+                  description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                  created_at: '5/22/2021',
+                  logo: 'logo.jpg',
+                  city: 'Roma',
+                  contract: 'Full Time'
+                }, 
+                {
+                    id: 2,
+                    company: 'Secundis',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Napoli',
+                    contract: 'Full Time'
+                  }, 
+                {
+                    id: 3,
+                    company: 'Stabiliantis',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Firenze',
+                    contract: 'Full Time'
+                  }, 
+            ],
+            starred: [1, 2, 3],
+            applied: [4, 5]
+            
+        }//chiude il data
+
+})//chiude la vue
