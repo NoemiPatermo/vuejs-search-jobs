fe6d211b1870d1272ca7174f1ee93e8e993c0892
noemi86-cpu <noemipatermo@gmail.com>
2021-07-06 20:51:52 +0200

    search-jobs

--- js/script.js
+++ js/script.js
@@ -17,9 +17,9 @@ new Vue (
                 {
                     id: 2,
                     company: 'Secundis',
-                    position: 'Developer',
+                    position: 'Python Developer',
                     description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
-                    created_at: '5/22/2021',
+                    created_at: '4/22/2021',
                     logo: 'logo.jpg',
                     city: 'Napoli',
                     contract: 'Full Time'
@@ -27,16 +27,16 @@ new Vue (
                 {
                     id: 3,
                     company: 'Strabiliantis',
-                    position: 'Developer',
+                    position: 'Full Stack Developer',
                     description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
-                    created_at: '5/22/2021',
+                    created_at: '1/22/2021',
                     logo: 'logo.jpg',
                     city: 'Firenze',
                     contract: 'Full Time'
                   }, 
             ],
             starred: [],
-            applied: [4, 5]
+            applied: []
             
         },//chiude il data
         methods: {
@@ -46,7 +46,24 @@ new Vue (
             removeToFav: function(starred,id) {
                 let index = starred.indexOf(id)
                 starred.splice(index, 1)
-            }
-        }
+            },
+            applyOn: function(applied,id) {
+                applied.push(id)
+            setTimeout(() => {
+                let modal = document.querySelector('.application-sent');   
+                let closeButton = document.querySelector('.application-sent .close');
+                modal.style.display = 'block'; 
+                closeButton.addEventListener('click', function () {
+                    modal.style.display = 'none';
+                })
+                }, 1000);
+            },
+            remove: function(applied,id){
+                let form = applied.indexOf(id)
+                applied.splice(form, 1)
+            },
+            
+            
+        }//chiude il methods
 
-})//chiude la vue
+});//chiude la vue
